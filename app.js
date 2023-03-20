1b54d18943c4c8d30e49cffa4b8a768896558644
Vladislav <118736029+dawvvlad@users.noreply.github.com>
2023-03-20 21:15:00 +0300

    Update app.js

--- app.js
+++ app.js
@@ -204,3 +204,23 @@ function printNums(event) {
         }, 1000)
     }
 }
+
+document.addEventListener('mousewheel', function(e){
+  if(!e.ctrlKey && !e.metaKey) return;
+
+  e.preventDefault();
+  e.stopImmediatePropagation();
+}, {passive:false});
+
+document.addEventListener('gesturestart', function(e){
+  e.preventDefault();
+  e.stopImmediatePropagation();
+}, {passive:false});
+
+document.addEventListener('keydown', function(e){
+  if(!e.ctrlKey && !e.metaKey) return;
+  if(e.keyCode != 189 && e.keyCode != 187) return;
+
+  e.preventDefault();
+  e.stopImmediatePropagation();
+}, {passive:false});
